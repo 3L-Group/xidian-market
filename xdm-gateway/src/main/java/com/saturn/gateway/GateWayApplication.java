242a58836e9bebe4988a4f3e5995a0e300036dea
QiuYiliang <1162922438@qq.com>
2021-03-26 10:58:47 +0800

    网关配置

--- xdm-gateway/src/main/java/com/saturn/gateway/GateWayApplication.java
+++ xdm-gateway/src/main/java/com/saturn/gateway/GateWayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 */
 @SpringBootApplication
 public class GateWayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class,args);
     }
